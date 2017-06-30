7fdd7c6180b6a7d16c5e7d1dc00301502f7c692c
Sagendar\Jethif <domoker@hotmail.com>
2017-06-30 13:15:47 +0200

    Added Demo Data

--- Project/src/Model/Row.java
+++ Project/src/Model/Row.java
@@ -9,4 +9,19 @@ public class Row {
     private ArrayList<Seat> seats;
     private String rowName;
 
+    public ArrayList<Seat> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(ArrayList<Seat> seats) {
+        this.seats = seats;
+    }
+
+    public String getRowName() {
+        return rowName;
+    }
+
+    public void setRowName(String rowName) {
+        this.rowName = rowName;
+    }
 }
