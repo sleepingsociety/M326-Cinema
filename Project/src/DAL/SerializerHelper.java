3afbe5a88a8d2b3e327eac6a5a08aac5b4b6547b
oriolgut <oriol.gut@noser.com>
2017-06-30 14:13:06 +0200

    Added serializerhelper

--- Project/src/DAL/SerializerHelper.java
+++ Project/src/DAL/SerializerHelper.java
@@ -1,7 +1,42 @@
 package DAL;
 
+import Model.Presentation;
+
+import java.io.*;
+
 /**
  * Created by oriol.gut on 16.06.2017.
  */
 public class SerializerHelper {
+
+    public void Serialize(Presentation presentation){
+        try {
+            FileOutputStream fileOut = new FileOutputStream("presentation.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(presentation);
+            out.close();
+            fileOut.close();
+        }catch(IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    public Presentation Deserialize(){
+        Presentation p = null;
+        try {
+            FileInputStream fileIn = new FileInputStream("/tmp/employee.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            p = (Presentation) in.readObject();
+            in.close();
+            fileIn.close();
+            return p;
+        }catch(IOException i) {
+            i.printStackTrace();
+            return null;
+        }catch(ClassNotFoundException c) {
+            System.out.println("Employee class not found");
+            c.printStackTrace();
+            return null;
+        }}
+
 }
